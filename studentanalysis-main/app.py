ea8eb91f9884847df80ab5cb0c2bdb4c3110eaf4
romeo-i43 <92739610+romeo-i43@users.noreply.github.com>
2021-11-25 21:01:42 +0530

    Add files via upload

---
+++ studentanalysis-main/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from multiapp import MultiApp
+import plot, table, cplot, ctable, home
+
+app=MultiApp()
+
+app.add_app("HOME",home.app)
+app.add_app("table",table.app)
+app.add_app("customtable",ctable.app)
+
+app.run()
+
